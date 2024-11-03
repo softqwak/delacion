e90873f18c8734a3f5da7916a7ce472425efc406
Valentin Kuzichev <valyakuzichev2017@gmail.com>
2024-11-03 16:16:41 +0300

    add padges

--- app/main.py
+++ app/main.py
@@ -31,11 +31,45 @@ async def index(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("concerts.html", context)
 
+@app.get("/concert", response_class=HTMLResponse)
+async def index(request: Request, concert_id: int = None):
+    # Передаем контекст в шаблон
+    if concert_id:
+        context = {"request": request}
+        return templates.TemplateResponse("concert.html", context)
+    else:
+        context = {"request": request}
+        templates.TemplateResponse("concerts.html", context)
+
 @app.get("/myorders", response_class=HTMLResponse)
 async def index(request: Request):
     # Передаем контекст в шаблон
     context = {"request": request}
     return templates.TemplateResponse("myorders.html", context)
+
+@app.get("/account", response_class=HTMLResponse)
+async def index(request: Request):
+    # Передаем контекст в шаблон
+    context = {"request": request}
+    return templates.TemplateResponse("account.html", context)
+
+@app.get("/login", response_class=HTMLResponse)
+async def index(request: Request):
+    # Передаем контекст в шаблон
+    context = {"request": request}
+    return templates.TemplateResponse("login.html", context)
+
+@app.get("/register", response_class=HTMLResponse)
+async def index(request: Request):
+    # Передаем контекст в шаблон
+    context = {"request": request}
+    return templates.TemplateResponse("register.html", context)
+
+@app.get("/password_reset", response_class=HTMLResponse)
+async def index(request: Request):
+    # Передаем контекст в шаблон
+    context = {"request": request}
+    return templates.TemplateResponse("password_reset.html", context)
 
 # @app.get("/test")
 # async def read_test(request: Request, num: int = 0):
